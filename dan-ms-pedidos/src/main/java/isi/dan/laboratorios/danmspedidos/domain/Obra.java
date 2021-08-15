30e19548b2eb940006e0cf8df9d5dc38c4bbcb94
fio154 <51134863+fio154@users.noreply.github.com>
2021-08-15 13:36:29 -0300

    Se agrega implementacion de base de datos con jpa

--- dan-ms-pedidos/src/main/java/isi/dan/laboratorios/danmspedidos/domain/Obra.java
+++ dan-ms-pedidos/src/main/java/isi/dan/laboratorios/danmspedidos/domain/Obra.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Obra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
 	private String descripcion;
 }
--- dan-ms-pedidos/src/main/java/isi/dan/laboratorios/danmspedidos/domain/Pedido.java
+++ dan-ms-pedidos/src/main/java/isi/dan/laboratorios/danmspedidos/domain/Pedido.java
@@ -4,18 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.Instant;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Pedido {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
 	private Instant fechaPedido;
+
+	@OneToMany
     private List<DetallePedido> detallesPedido;
+
+	@OneToOne
     private EstadoPedido estado;
+
+	@OneToOne
     private Obra obra;
 
 	public void setItemDetallePedido(DetallePedido detallePedido) {
