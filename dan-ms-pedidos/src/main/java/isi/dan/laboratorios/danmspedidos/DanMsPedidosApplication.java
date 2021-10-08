aca67cad0aa0cf459c0c23822445d2d51b777200
Ale Guiter <aleguiter9@gmail.com>
2021-10-08 17:58:49 -0300

    Added eureka client in main

--- dan-ms-pedidos/src/main/java/isi/dan/laboratorios/danmspedidos/DanMsPedidosApplication.java
+++ dan-ms-pedidos/src/main/java/isi/dan/laboratorios/danmspedidos/DanMsPedidosApplication.java
@@ -2,8 +2,10 @@ package isi.dan.laboratorios.danmspedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DanMsPedidosApplication {
 
 	public static void main(String[] args) {
